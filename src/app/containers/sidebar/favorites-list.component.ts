9c75e9713e625507b3e624306b097fe6d15b1fe6
marina-barbosa <marinabarbosa.exp@gmail.com>
2024-10-25 06:26:07 -0300

    feat: initialize app structure with components, containers, and services

---
+++ src/app/containers/sidebar/favorites-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-favorites-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './favorites-list.component.html',
+  styleUrl: './favorites-list.component.scss'
+})
+export class FavoritesListComponent {
+
+}
