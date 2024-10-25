022d7aca92a84a79cd6d61c61d0c02eb22403012
marina-barbosa <marinabarbosa.exp@gmail.com>
2024-10-25 07:07:17 -0300

    feat: create button component with primary and warning styles

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-button',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './button.component.html',
   styleUrl: './button.component.scss'
 })
 export class ButtonComponent {
-
+  @Input() variant: 'primary' | 'warning' = 'primary';
 }
