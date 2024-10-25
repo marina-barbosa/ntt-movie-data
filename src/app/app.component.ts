501c792c47872bad24e7676fa66bd78bfa53f10d
marina-barbosa <marinabarbosa.exp@gmail.com>
2024-10-25 08:56:13 -0300

    feat: add responsive footer with social media links

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonComponent } from "./components/button/button.component";
 import { HeaderComponent } from "./layout/header/header.component";
+import { FooterComponent } from "./layout/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonComponent, HeaderComponent],
+  imports: [RouterOutlet, ButtonComponent, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
