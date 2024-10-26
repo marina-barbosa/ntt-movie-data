170c1693ac44e71e4c6f2082e008094b8d27aa0d
marina-barbosa <marinabarbosa.exp@gmail.com>
2024-10-26 13:20:31 -0300

    deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: process.env["NG_APP_FIREBASE_API_KEY"],
-    authDomain: process.env["NG_APP_FIREBASE_AUTH_DOMAIN"],
-    projectId: process.env["NG_APP_FIREBASE_PROJECT_ID"],
-    storageBucket: process.env["NG_APP_FIREBASE_STORAGE_BUCKET"],
-    messagingSenderId: process.env["NG_APP_FIREBASE_MESSAGING_SENDER_ID"],
-    appId: process.env["NG_APP_FIREBASE_APP_ID"]
+    apiKey: "AIzaSyA3TbFcXyvfJsrExzDLSHJyOACDOuIR5E4",
+    authDomain: "ntt-movie-data.firebaseapp.com",
+    projectId: "ntt-movie-data",
+    storageBucket: "ntt-movie-data.appspot.com",
+    messagingSenderId: "890334908927",
+    appId: "1:890334908927:web:3d7cf475034d45311c17ca"
   }
 };
